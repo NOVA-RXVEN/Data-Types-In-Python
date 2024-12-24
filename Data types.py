7c19038aec1f3aa65865e71d443ff41411e27005
Liban Tahsin <tahsin.liban0927@gmail.com>
2024-12-24 11:35:55 +0600

    first commit

---
+++ Data types.py
@@ -0,0 +1,11 @@
+name="Giga"
+print(type(name))
+
+age=6500
+print(type(age))
+
+height=15.6
+print(type(height))
+
+isStudent = True
+print(type(isStudent))
---
+++ String Operations.py
@@ -0,0 +1,13 @@
+str = "Rayan"
+
+print(str[2])
+
+str2 = "Hello Mr. "
+
+print(str2 + str)
+
+print(str[0:4:1])
+
+revText = str[::-1]
+
+print(revText)
